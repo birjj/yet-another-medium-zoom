82f40a70161c98473cdac784bb5de17a9706a667
birjolaxew <johanringmann@gmail.com>
2019-11-20 10:46:04 +0100

    Update Rollup and Jest configurations
    Now supports importing CSS

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,26 +1,28 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
-import typescript from 'rollup-plugin-typescript2'
-import json from 'rollup-plugin-json'
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import sourceMaps from "rollup-plugin-sourcemaps";
+import typescript from "rollup-plugin-typescript2";
+import json from "rollup-plugin-json";
+import postcss from "rollup-plugin-postcss";
 
-const pkg = require('./package.json')
+const pkg = require("./package.json")
 
-const libraryName = 'medium-lightbox'
+const libraryName = "yet-another-medium-zoom"
 
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
+    { file: pkg.main, name: "yamz", format: "umd", sourcemap: true },
+    { file: pkg.module, format: "es", sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
   external: [],
   watch: {
-    include: 'src/**',
+    include: "src/**",
   },
   plugins: [
+    // Allow css resolution
+    postcss({ plugins: [] }),
     // Allow json resolution
     json(),
     // Compile TypeScript files
