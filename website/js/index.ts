bc6bc8cca1b1d43d3e6ba8ab1e4dc1157d918796
birjolaxew <johanringmann@gmail.com>
2019-11-26 08:12:03 +0100

    Update website with more fitting images

--- website/js/index.ts
+++ website/js/index.ts
@@ -1,4 +1,5 @@
 import lightbox from "../../src/core";
 
-const $imgs = Array.from(document.querySelectorAll("img"));
-lightbox.bind($imgs);
+const $imgs = Array.from(document.querySelectorAll("*:not(picture) > img, picture"));
+console.log("Binding to", $imgs);
+lightbox.bind($imgs as HTMLElement[], { duration: 3000 });
