4b07b321da36751f8a6e97bb3f59bac7adb50086
birjolaxew <johanringmann@gmail.com>
2019-12-31 08:42:05 +0100

    Move swipe detection into its own component

---
+++ src/swipe/detector.ts
@@ -0,0 +1,164 @@
+type coordinates = {
+    x: number,
+    y: number,
+};
+type listeners = {
+    "start"?: (position: coordinates, metadata?: any) => void,
+    "update"?: (offset: coordinates, metadata?: any) => void,
+    "end"?: (direction: "left"|"right", metadata?: any) => void,
+    "cancel"?: (metadata?: any) => void,
+};
+
+export default class SwipeDetector {
+    listeners: listeners;
+    state?: {
+        startTouch: coordinates & { time: number },
+        lastTouch: coordinates & { time: number },
+        identifier?: number,
+        velocity: number,
+        isSwipe: boolean,
+        metadata?: any
+    };
+    threshold: number;
+
+    constructor(listeners: listeners, threshold: number = 0) {
+        this.listeners = listeners;
+        this.threshold = threshold;
+
+        // make sure we can be attached as event listeners easily
+        this.start = this.start.bind(this);
+        this.move = this.move.bind(this);
+        this.end = this.end.bind(this);
+        this.cancel = this.cancel.bind(this);
+    }
+    setThreshold(threshold: number) { this.threshold = threshold; }
+
+    start(e: MouseEvent|TouchEvent, metadata?: any) {
+        if (!(e instanceof MouseEvent)) {
+            // we ignore any start touches if another finger is currently down (and cancel any active ones if a new finger is touched)
+            if (e.touches.length > 1) {
+                this.cancel();
+                return false;
+            }
+        }
+        const touch = e instanceof MouseEvent ? e : e.touches[0];
+
+        const initialTouch = {
+            x: touch.clientX,
+            y: touch.clientY,
+            time: Date.now(),
+        };
+        this.state = {
+            startTouch: { ...initialTouch },
+            lastTouch: { ...initialTouch },
+            isSwipe: false,
+            velocity: 0,
+            identifier: touch instanceof MouseEvent ? undefined : touch.identifier,
+            metadata: metadata,
+        };
+
+        this.emit("start", initialTouch, metadata);
+    }
+
+    move(e: MouseEvent|TouchEvent) {
+        if (!this.state) { return; } // ignore if we aren't currently dragging
+        let touch: MouseEvent|Touch|undefined = e instanceof MouseEvent ? e : undefined;
+        if (!(e instanceof MouseEvent)) {
+            for (let i = 0; i < e.changedTouches.length; ++i) {
+                const t = e.changedTouches[i];
+                if (t.identifier === this.state.identifier) {
+                    touch = t;
+                    break;
+                }
+            }
+        }
+        if (!touch) { return; } // ignore if none of the touches are the one we're tracking
+
+        const timeDelta = Date.now() - this.state.lastTouch.time;
+        if (timeDelta < 16) { return; } // debounce
+        // we compare the direction relative to the starting point; if it's more down than up, we cancel the drag ('cause it's probably a scroll)
+        const xDelta = touch.clientX - this.state.startTouch.x;
+        const yDelta = touch.clientY - this.state.startTouch.y;
+        const absXDelta = Math.abs(xDelta);
+        const absYDelta = Math.abs(yDelta);
+
+        // ignore ones where the mouse hasn't moved
+        if (absXDelta === 0 && absYDelta === 0) { return; }
+
+        // update our state
+        this.state.lastTouch.x = touch.clientX;
+        this.state.lastTouch.y = touch.clientY;
+        this.state.lastTouch.time = Date.now();
+        this.state.velocity = timeDelta
+            ? (touch.clientX - this.state.lastTouch.x) / timeDelta
+            : 0;
+
+        const IGNORE_THRESHOLD = 32; // ignore test if we have moved barely anything. Possibly consider if this is better as an option?
+        if (!this.state.isSwipe) {
+            // don't start tracking as a swipe until after we've exceeding our threshold
+            if (Math.max(absXDelta, absYDelta) < IGNORE_THRESHOLD) {
+                return;
+            } else {
+                // if we've just now gone past our threshold, check if this looks more like a scroll than a swipe
+                if (absYDelta > absXDelta) {
+                    this.cancel();
+                    return;
+                }
+                this.state.isSwipe = true;
+            }
+        }
+
+        this.emit("update", { x: xDelta, y: yDelta }, this.state.metadata);
+    }
+
+    end(e: MouseEvent|TouchEvent) {
+        if (!this.state) { return false; } // ignore if we aren't currently dragging
+        let touch: MouseEvent|Touch|undefined = e instanceof MouseEvent ? e : undefined;
+        if (!(e instanceof MouseEvent)) {
+            for (let i = 0; i < e.changedTouches.length; ++i) {
+                const t = e.changedTouches[i];
+                if (t.identifier === this.state.identifier) {
+                    touch = t;
+                    break;
+                }
+            }
+        }
+        if (!touch) { return false; } // ignore if none of the touches are the one we're tracking
+
+        // then calculate the projected point we want to check
+        const velocity = this.state.velocity;
+        const totalDelta = touch.clientX - this.state.startTouch.x;
+        const recentDelta = touch.clientX - this.state.lastTouch.x;
+        let finalDelta = totalDelta;
+        // we only project a point out if the flick is in the direction of the swipe, otherwise we just use the touch position
+        if ((totalDelta >= 0 && recentDelta >= 0 || totalDelta <= 0 && recentDelta <= 0)) {
+            finalDelta += velocity * 50; // project it 50ms out
+        }
+
+        // finally check if it's a swipe
+        if (this.threshold && this.threshold > 0 && Math.abs(finalDelta) > this.threshold) {
+            this.emit("end", finalDelta < 0 ? "left" : "right", this.state.metadata);
+            this.after();
+            return true;
+        }
+        this.cancel();
+        return false;
+    }
+
+    cancel() {
+        if (!this.state) { return; }
+        this.emit("cancel", this.state.metadata);
+        this.after();
+    }
+
+    after() {
+        delete this.state;
+    }
+
+    emit(event: keyof listeners, ...args: any[]) {
+        const listener = this.listeners[event] as ((...args: any[]) => void)|undefined;
+        if (listener) {
+            listener(...args);
+        }
+    }
+}
--- src/swipe/swipe.ts
+++ src/swipe/swipe.ts
@@ -2,25 +2,15 @@ import { MediumLightboxCore } from "../core";
 import { YamzPlugin, Classes } from "../types";
 import withAlbum from "../album/album";
 import "./swipe.css";
+import SwipeDetector from "./detector";
 
 export interface Swipeable {
-    startSwipe: (e: Touch|MouseEvent, opts: SwipeOptions) => void,
-    updateSwipe: (e: Touch|MouseEvent, opts: SwipeOptions) => void,
-    endSwipe: (e: Touch|MouseEvent, opts: SwipeOptions) => boolean,
-    cancelSwipe: () => void,
+    swipeDetector: SwipeDetector,
+    applySwipeTransform: (offset: {x:number,y:number}, opts: SwipeOptions) => void,
+    onSwipeStart: (point: {x:number,y:number}, opts: SwipeOptions) => void,
+    onSwipeEnd: (direction: "left"|"right", opts: SwipeOptions) => void,
+    onSwipeCancel: (opts: SwipeOptions) => void,
     afterSwipe: () => void,
-    applySwipeTransform: (deltaX: number, opts: SwipeOptions) => void,
-    _startTouch?: {
-        clientX: number,
-        clientY: number,
-        identifier?: number
-    },
-    _lastTouch?: {
-        clientX: number,
-        clientY: number,
-        velocityX: number,
-        time: number
-    }
 };
 
 export interface SwipeOptions {
@@ -50,22 +40,31 @@ export default function withSwipe<YamzType extends ReturnType<typeof withAlbum>>
 
         if (!opts.album) { return $lightbox; }
 
+        if (opts.swipeThreshold) {
+            yamz.swipeDetector.setThreshold(opts.swipeThreshold);
+        }
+
         // normally we don't let mouse users swipe, but developers can enable it if they want
         if (opts.swipeOnDesktop) {
             $lightbox.addEventListener("mousedown", e => {
                 e.preventDefault();
-                this.startSwipe(e, opts);
+                yamz.swipeDetector.start(e, opts);
             });
             $lightbox.addEventListener("mousemove", e => {
                 e.preventDefault();
-                this.updateSwipe(e, opts);
+                yamz.swipeDetector.move(e);
             });
             let justSwiped = 0;
             $lightbox.addEventListener("mouseup", e => {
                 e.preventDefault();
-                justSwiped = this.endSwipe(e, opts)
-                    ? Date.now()
-                    : 0;
+                if (yamz.swipeDetector.state) {
+                    justSwiped = yamz.swipeDetector.state.isSwipe
+                        ? Date.now()
+                        : 0;
+                }
+                if (!yamz.swipeDetector.end(e)) {
+                    this.onSwipeCancel(opts);
+                };
             });
             $lightbox.addEventListener("click", e => {
                 // we stop propagation here so that the lightbox doesn't close if we just swiped
@@ -76,10 +75,10 @@ export default function withSwipe<YamzType extends ReturnType<typeof withAlbum>>
         }
 
         // we always attach listeners for touch
-        $lightbox.addEventListener("touchstart", e => handleTouchStart.call(this, e, opts));
-        $lightbox.addEventListener("touchmove", e => handleTouchMove.call(this, e, opts));
-        $lightbox.addEventListener("touchend", e => handleTouchEnd.call(this, e, opts));
-        $lightbox.addEventListener("touchcancel", e => handleTouchEnd.call(this, e, opts));
+        $lightbox.addEventListener("touchstart", e => { yamz.swipeDetector.start(e, opts); });
+        $lightbox.addEventListener("touchmove", yamz.swipeDetector.move);
+        $lightbox.addEventListener("touchend", yamz.swipeDetector.end);
+        $lightbox.addEventListener("touchcancel", yamz.swipeDetector.cancel);
 
         return $lightbox;
     };
@@ -91,15 +90,15 @@ export default function withSwipe<YamzType extends ReturnType<typeof withAlbum>>
         return outp;
     };
 
-    yamz.applySwipeTransform = function(deltaX: number, opts: SwipeOptions) {
+    yamz.applySwipeTransform = function(touchOffset: { x: number, y: number }, opts: SwipeOptions) {
         if (!this.active) { return; }
-        let offset = deltaX;
+        let offset = Math.abs(touchOffset.x);
         let scale = 1;
         if (opts.swipeResponseLimit) {
             // use a sine function to slowly scale down
             // sine wave is 3 long, because that gives a 45 degree angle at the start, so 1px touch movement = 1px image movement
             const limit = opts.swipeResponseLimit * 1.5; // this is where we want the image to stop moving entirely
-            const progress = Math.abs(deltaX) / opts.swipeResponseLimit;
+            const progress = Math.abs(touchOffset.x) / opts.swipeResponseLimit;
             const clampedProgress = Math.min(progress, 1.5);
             const offsetScale = Math.sin(clampedProgress * Math.PI / 3);
             offset = opts.swipeResponseLimit * offsetScale;
@@ -110,10 +109,10 @@ export default function withSwipe<YamzType extends ReturnType<typeof withAlbum>>
             }
 
             // maintain a very slight response to dragging further
-            const linearOffset = Math.abs(deltaX) - Math.min(Math.abs(deltaX), limit);
+            const linearOffset = Math.abs(touchOffset.x) - Math.min(Math.abs(touchOffset.x), limit);
             offset += linearOffset * 0.05;
 
-            if (deltaX < 0) { offset = -offset; }
+            if (touchOffset.x < 0) { offset = -offset; }
         }
 
         const $target = this.active.$lightbox.querySelector(`.${Classes.IMG_WRAPPER}`) as HTMLElement|null;
@@ -123,36 +122,7 @@ export default function withSwipe<YamzType extends ReturnType<typeof withAlbum>>
         }
     };
 
-    /**
-     * When a touch is started, we simply set it as the active touch
-     * In the case where an touch starts while we have an active touch, we cancel everything (this is used for e.g. zoom)
-     */
-    function handleTouchStart(this: typeof yamz, e: TouchEvent, opts: SwipeOptions) {
-        const touches = e.changedTouches;
-        // only let through if we aren't currently swiping
-        if (e.touches.length > 1) {
-            // we also ignore any start touches if another finger is currently down (and cancel any active ones if a new finger is touched)
-            return this.cancelSwipe();
-        }
-        if (!this._lastTouch && e.touches.length == 1) {
-            return this.startSwipe(touches[0], opts);
-        }
-    }
-    yamz.startSwipe = function(e: Touch|MouseEvent, opts: SwipeOptions) {
-        this._startTouch = {
-            clientX: e.clientX,
-            clientY: e.clientY,
-            identifier: "identifier" in e
-                        ? e.identifier
-                        : undefined
-        };
-        this._lastTouch = {
-            clientX: e.clientX,
-            clientY: e.clientY,
-            time: Date.now(),
-            velocityX: 0
-        };
-
+    yamz.onSwipeStart = function(point: {x:number,y:number}, opts: SwipeOptions) {
         if (this.active) {
             const $target = this.active.$lightbox.querySelector(`.${Classes.IMG_WRAPPER}`);
             if ($target) {
@@ -160,114 +130,22 @@ export default function withSwipe<YamzType extends ReturnType<typeof withAlbum>>
             }
         }
     };
-
-    /**
-     * Handles updating the DOM to match the swipe. If the user moves too far up/down, the swipe is cancelled
-     */
-    function handleTouchMove(this: typeof yamz, e: TouchEvent, opts: SwipeOptions) {
-        const touches = e.changedTouches;
-        // only let through if this is our currently active touch
-        if (!this._startTouch) { return; }
-        for (let i = 0; i < touches.length; ++i) {
-            const touch = touches[i];
-            if (touch.identifier === this._startTouch.identifier) {
-                return this.updateSwipe(touch, opts);
-            }
+    yamz.onSwipeEnd = function(direction: "left"|"right", opts: SwipeOptions) {
+        if (this.active) {
+            const $btn = direction === "left"
+                ? this.active.$lightbox.querySelector(".yamz__album__next")
+                : this.active.$lightbox.querySelector(".yamz__album__prev");
+            if ($btn) { ($btn as HTMLElement).click(); }
         }
-    }
-    yamz.updateSwipe = function(e: Touch|MouseEvent, opts: SwipeOptions) {
-        if (!this._startTouch || !this._lastTouch) { return; }
-        const timeDelta = Date.now() - this._lastTouch.time;
-        if (timeDelta < 16) { return; } // debounce
-        // we compare the direction relative to the starting point; if it's more down than up, we cancel the drag ('cause it's probably a scroll)
-        const xDelta = Math.abs(e.clientX - this._startTouch.clientX);
-        const yDelta = Math.abs(e.clientY - this._startTouch.clientY);
-
-        this._lastTouch.clientX = e.clientX;
-        this._lastTouch.clientY = e.clientY;
-        this._lastTouch.time = Date.now();
-        this._lastTouch.velocityX = timeDelta ? (e.clientX - this._startTouch.clientX) / timeDelta : 0;
-
-        const IGNORE_THRESHOLD = 32; // ignore test if we have moved barely anything. Possibly consider if this is better as an option?
-        if (Math.max(xDelta, yDelta) < IGNORE_THRESHOLD) {
-            return;
-        }
-
-        if (yDelta > xDelta) {
-            return this.cancelSwipe();
-        }
-
-        // ignore ones where the mouse hasn't moved
-        if (xDelta === 0 && yDelta === 0) { return; }
-
-        this.applySwipeTransform(e.clientX - this._startTouch.clientX, opts);
+        this.afterSwipe();
     };
 
-    /**
-     * Handles detecting the swipe once the touch ends
-     * The velocity of the movement is used to project a point 0.5s out in the future
-     * This lets quick movements that "throw" the entry out of the way be detected, along with slower drags
-     */
-    function handleTouchEnd(this: typeof yamz, e: TouchEvent, opts: SwipeOptions) {
-        const touches = e.changedTouches;
-        // only let through if this is our currently active touch
-        if (!this._startTouch) { return; }
-        for (let i = 0; i < touches.length; ++i) {
-            const touch = touches[i];
-            if (touch.identifier === this._startTouch.identifier) {
-                if (this.endSwipe(touch, opts)) {
-                    e.preventDefault();
-                    return;
-                }
-            }
-        }
-    }
-    /** Returns true/false depending on whether movement was a swipe */
-    yamz.endSwipe = function(e: Touch|MouseEvent, opts: SwipeOptions) {
-        if (!this._startTouch || !this._lastTouch) { return false; }
-
-        // make sure we have an up to date velocity measurement
-        let velocity = this._lastTouch.velocityX;
-        const timeDelta = Date.now() - this._lastTouch.time;
-        if (timeDelta > 16) {
-            velocity = (e.clientX - this._lastTouch.clientX) / timeDelta;
-        }
-
-        // then calculate the projected point we want to check
-        const totalDelta = e.clientX - this._startTouch.clientX;
-        const recentDelta = e.clientX - this._lastTouch.clientX;
-        let finalDelta = totalDelta;
-        // we only project a point out if the flick is in the direction of the swipe, otherwise we just use the touch position
-        if ((totalDelta >= 0 && recentDelta >= 0 || totalDelta <= 0 && recentDelta <= 0)) {
-            finalDelta += velocity * 50;
-        }
-
-        delete this._startTouch;
-        delete this._lastTouch;
-
-        // finally check it
-        if (opts.swipeThreshold && opts.swipeThreshold > 0 && Math.abs(finalDelta) > opts.swipeThreshold) {
-            if (this.active) {
-                const $btn = finalDelta < 0
-                    ? this.active.$lightbox.querySelector(".yamz__album__next")
-                    : this.active.$lightbox.querySelector(".yamz__album__prev");
-                if ($btn) { ($btn as HTMLElement).click(); }
-            }
-            this.afterSwipe();
-            return true;
-        }
-        this.cancelSwipe();
-        return false;
-    };
-
-    yamz.cancelSwipe = function() {
-        this.applySwipeTransform(0, {});
+    yamz.onSwipeCancel = function(opts: SwipeOptions) {
+        this.applySwipeTransform({ x: 0, y: 0 }, opts);
         this.afterSwipe();
     };
 
     yamz.afterSwipe = function() {
-        delete this._startTouch;
-        delete this._lastTouch;
         if (this.active) {
             const $target = this.active.$lightbox.querySelector(`.${Classes.IMG_WRAPPER}`);
             if ($target) {
@@ -275,6 +153,13 @@ export default function withSwipe<YamzType extends ReturnType<typeof withAlbum>>
             }
         }
     };
+
+    yamz.swipeDetector = new SwipeDetector({
+        start: yamz.onSwipeStart.bind(yamz),
+        update: yamz.applySwipeTransform.bind(yamz),
+        end: yamz.onSwipeEnd.bind(yamz),
+        cancel: yamz.onSwipeCancel.bind(yamz)
+    });
 
     return yamz;
 };
